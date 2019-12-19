f9ffa640463893198e40d9bc6424b7722b92a1fc
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-19 03:18:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog 
   
+  def name=(new_dog)
+    @this_dogs_name = new_dog
+  end
+  
+  def name
+    @this_dogs_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
 end
+
+fido = Dog.new_dog
+
+fido.name
+
+fido.bark
